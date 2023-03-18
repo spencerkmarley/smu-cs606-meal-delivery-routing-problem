c937a7bd383266469ce9ce31bbd7c610fe5d87b1
micheal.loh <micheal.loh@bytedance.com>
2023-03-18 17:52:25 +0800

    updated docstring:

--- functions/travel_time.py
+++ functions/travel_time.py
@@ -1,9 +1,19 @@
 import numpy as np
+import pandas as pd
 
-def travel_time(origin_id, destination_id, meters_per_minute, locations):
-    '''
-    Calculate travel time between two points, the origin and the destination
-    '''
+def travel_time(origin_id : str, destination_id : str , meters_per_minute : int, locations : pd.DataFrame):
+    """
+    Calculate the travel time between two locations.
+    Args:
+        origin_id (int): The id of the origin location.
+        destination_id (int): The id of the destination location.
+        meters_per_minute (float): The average speed of the vehicle in meters per minute.
+        locations (DataFrame): The DataFrame containing the locations.
+
+    Returns:
+        float: The travel time between the origin and destination in minutes.
+    :param locations:
+    """
 
     dist = np.sqrt((locations.at[destination_id, 'x'] - locations.at[origin_id, 'x'])**2\
                 + (locations.at[destination_id, 'y'] - locations.at[origin_id, 'y'])**2) # calculate the distance between the origin and the destination
