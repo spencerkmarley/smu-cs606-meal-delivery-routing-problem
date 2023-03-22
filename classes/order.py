6da22262e1f2df2701cc39cd05ade6f0a05c3999
Spencer Marley <spencermarley@gmail.com>
2023-03-22 15:36:27 +0800

    Updates to comments and formatting

--- classes/order.py
+++ classes/order.py
@@ -13,8 +13,7 @@ class Order:
         self.placement_time = order_information.get('placement_time') # order placement time
         self.restaurant_id = order_information.get('restaurant') # restaurant id
         self.ready_time = order_information.get('ready_time') # order ready time
-        
-        # To be updated after assignment:
         self.pickup_time = 0 # order pickup time
         self.dropoff_time = 0 # order dropoff time
         self.courier_id = "" # courier id
+        self.assign_time = 0 # assignment time
